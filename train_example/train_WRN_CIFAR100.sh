7891ad5a4baca567207f10a10d7d1794358c132e
JohannaDK <johannadciofalo@gmail.com>
2024-11-08 12:45:27 +0000

    training WRN for CIFAR100

---
+++ train_example/train_WRN_CIFAR100.sh
@@ -0,0 +1,12 @@
+cd /home/johannakhodaverdian/DD2412-Final-Project/
+export PYTHONPATH=$PWD
+echo "!!Training model!!"
+python3 src/experiments/00_train_models.py \
+    --model WRN \
+    --epochs 600 \
+    --accelerator gpu \
+    --seed 1 \
+    --dataset CIFAR100 \
+    --model_name CIFAR100_WRN_28_10_Base \
+    --batch_size 256
+echo "!!Training done!!"
