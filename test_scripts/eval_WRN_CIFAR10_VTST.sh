c80b14e422ff6566616158f27ee7750f63731928
Eric Banzuzi <banzuzi@kth.se>
2024-11-14 12:26:16 +0000

    Evaluation of VTST on CIFAR10

--- test_scripts/eval_WRN_CIFAR10_VTST.sh
+++ test_scripts/eval_WRN_CIFAR10_VTST.sh
@@ -4,7 +4,7 @@ cd /home/ericbanzuzi/DD2412-Final-Project/
 export PYTHONPATH=$PWD
 echo "!!Evaluating model!!"
 python3 src/experiments/01_eval_models.py \
-    --save_file_name WRN_CIFAR10_VTST.txt \
-    --model_name_file evaluate_wrn_cifar10_vtst.txt \
-    --num_samples 10
+    --save_file_name WRN_CIFAR10_VTST_M=1.txt \
+    --model_name_file evaluate_wrn_cifar10_vtst_m=1.txt \
+    --num_samples 1
 echo "!!Evaluation done!!"
