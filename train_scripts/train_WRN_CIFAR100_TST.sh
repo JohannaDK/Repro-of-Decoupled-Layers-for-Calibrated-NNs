ed713570db19376421ebf5fe67d71f3d067c2904
JohannaDK <johannadciofalo@gmail.com>
2024-11-21 05:38:08 +0000

    WRN CIFAR 100 Z512 TST  eval done

--- train_scripts/train_WRN_CIFAR100_TST.sh
+++ train_scripts/train_WRN_CIFAR100_TST.sh
@@ -5,11 +5,11 @@ export PYTHONPATH=$PWD
 echo "!!Training model!!"
 python3 src/experiments/00_train_models.py \
     --freeze_qyx \
-    --model_name TST_CIFAR100_Z128 \
+    --model_name TST_CIFAR100_Z512 \
     --model TST \
     --epochs 40 \
     --accelerator gpu \
-    --latent_dim 128 \
+    --latent_dim 512 \
     --seed 0 \
     --pretrained_qyx experiment_results/table_metrics/CIFAR100_WRN/checkpoints/seed=1-epoch=18-valid_loss=1.939-model_name=WRN_CIFAR100_28_10_Base_seed1.ckpt \
     --dataset CIFAR100 \
