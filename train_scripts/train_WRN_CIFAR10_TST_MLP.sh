648c1ff664319830c07e860b0a1363671ff7ae99
Eric Banzuzi <banzuzi@kth.se>
2024-11-22 11:29:51 +0000

    TST results with MLP size 4 and Z128

--- train_scripts/train_WRN_CIFAR10_TST_MLP.sh
+++ train_scripts/train_WRN_CIFAR10_TST_MLP.sh
@@ -5,7 +5,7 @@ export PYTHONPATH=$PWD
 echo "!!Training model!!"
 python3 src/experiments/00_train_models.py \
     --freeze_qyx \
-    --model_name TSTEXP_CIFAR10_MLP4 \
+    --model_name TSTEXP_CIFAR10_MLP4_Z128 \
     --model TSTEXP \
     --epochs 40 \
     --accelerator gpu \
@@ -13,5 +13,6 @@ python3 src/experiments/00_train_models.py \
     --pretrained_qyx experiment_results/CIFAR10_WRN/checkpoints/seed=1-epoch=586-valid_loss=0.450-model_name=WRN_CIFAR10_28_10_Base_seed1.ckpt \
     --dataset CIFAR10 \
     --seeds_per_job 10 \
-    --mlp_size 4
+    --mlp_size 4 \
+    --latent_dim 128
 echo "!!Training done!!"
