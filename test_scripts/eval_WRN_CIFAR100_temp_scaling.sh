8721a975da9f9e5b7d9c33a33f74d2874bc03506
JohannaDK <johannadciofalo@gmail.com>
2024-11-26 10:02:51 +0000

    WRN CIFAR100 Temperature Scaling done

---
+++ test_scripts/eval_WRN_CIFAR100_temp_scaling.sh
@@ -0,0 +1,10 @@
+#!/bin/bash	
+
+cd /home/johannakhodaverdian/DD2412-Final-Project/
+export PYTHONPATH=$PWD
+echo "!!Evaluating model!!"
+python3 src/experiments/01_eval_models.py \
+    --save_file_name WRN_CIFAR100_TS.txt \
+    --model_name_file evaluate_wrn_cifar100.txt \
+    --temperature_scale
+echo "!!Evaluation done!!"
