1bc27c42b37959c00f5f462e8da67cdab12c31cc
JohannaDK <johannadciofalo@gmail.com>
2024-12-05 10:30:28 +0000

    EfficientNet50 TST on CIFAR10

--- src/experiments/00_train_models.py
+++ src/experiments/00_train_models.py
@@ -10,8 +10,8 @@ from src.models.TST import *
 from src.models.VTST import *
 from src.models.WRN import *
 from src.models.CNN import *
-from src.models.ResNet import ResNet50
 from src.models.ResNet_new import *
+from src.models.EfficientNet import *
 import torch.utils.data as data
 from argparse import ArgumentParser
 from torch.utils.data import DataLoader
@@ -108,8 +108,6 @@ for i in range(args.seeds_per_job):
 
     if args.model == "WRN" and (args.dataset == "SVHN" or args.dataset=="CIFAR100" or args.dataset=="CIFAR10"):
         model = WideResNet(num_classes=num_classes, depth=28, width=10, num_input_channels=3)
-    elif args.model == "ResNet50" and (args.dataset == "SVHN" or args.dataset=="CIFAR100" or args.dataset=="CIFAR10"):
-        model = ResNet50()
     elif (args.dataset == "SVHN" or args.dataset =="CIFAR10" or args.dataset=="CIFAR100") and args.model=="TST" and args.pretrained_qyx is not None:
         model = TST(dataset=args.dataset, num_classes=num_classes, latent_dim=args.latent_dim, accelerator=args.accelerator, pretrained_qyx=load_WRN_model(args.pretrained_qyx, dataset=args.dataset), separate_body=True)
     elif (args.dataset == "SVHN" or args.dataset =="CIFAR10" or args.dataset=="CIFAR100") and args.model=="TST" and args.pretrained_qyx is None:
@@ -118,6 +116,14 @@ for i in range(args.seeds_per_job):
         model = VTST(dataset=args.dataset, num_classes=num_classes, latent_dim=args.latent_dim, accelerator=args.accelerator, bound_qzx_var=True, pretrained_qyx=load_WRN_model(args.pretrained_qyx, dataset=args.dataset), separate_body=True)
     elif (args.dataset == "SVHN" or args.dataset =="CIFAR10" or args.dataset=="CIFAR100") and args.model=="VTST" and args.pretrained_qyx is None:
         model = VTST(dataset=args.dataset, num_classes=num_classes, latent_dim=args.latent_dim, accelerator=args.accelerator, bound_qzx_var=True, pretrained_qyx=None, separate_body=True)
+    elif args.model == "ResNet50" and (args.dataset == "SVHN" or args.dataset=="CIFAR100" or args.dataset=="CIFAR10"):
+        model = ResNet50(num_classes=num_classes)
+    elif (args.dataset == "SVHN" or args.dataset =="CIFAR10" or args.dataset=="CIFAR100") and args.model=="TST_ResNet50" and args.pretrained_qyx is not None:
+        model = TST(dataset=args.dataset, num_classes=num_classes, latent_dim=args.latent_dim, accelerator=args.accelerator, pretrained_qyx=load_ResNet50_model(args.pretrained_qyx, dataset=args.dataset), separate_body=True, ResNet50_experiment = True)
+    elif (args.dataset == "SVHN" or args.dataset =="CIFAR10" or args.dataset=="CIFAR100") and args.model=="TST_ResNet50" and args.pretrained_qyx is None:
+        model = TST(dataset=args.dataset, num_classes=num_classes, latent_dim=args.latent_dim, accelerator=args.accelerator, pretrained_qyx=None, separate_body=True, ResNet50_experiment = True)
+    elif args.model == "EfficientNet" and (args.dataset == "SVHN" or args.dataset=="CIFAR100" or args.dataset=="CIFAR10"):
+        model = EfficientNetV2_S(num_classes)
     elif args.model=="CNN" and (args.dataset == "SVHN" or args.dataset =="CIFAR10" or args.dataset=="CIFAR100"):
         model = CNN(num_classes=num_classes)
     elif (args.dataset == "SVHN" or args.dataset =="CIFAR10" or args.dataset=="CIFAR100") and args.model=="TST_CNN" and args.pretrained_qyx is not None:
@@ -149,9 +155,9 @@ for i in range(args.seeds_per_job):
 
     gammas = [float(gamma) for gamma in args.gammas] if args.gammas else None
     probs = [float(prob) for prob in args.probs] if args.probs else None
-    if args.model =="WRN" or args.model == "CNN" or args.model == "VIT" or args.model == "ResNet50":
+    if args.model =="WRN" or args.model == "CNN" or args.model == "VIT" or args.model == "ResNet50" or args.model == "EfficientNet":
         lightning_module = lt_disc_models(model, num_classes, loss=args.loss, gammas=gammas, probs=probs)
-    elif args.model == "TST" or args.model == "TST_CNN" or args.model == "REINIT" or  args.model == "TSTEXP" or args.model == "TST_VIT":
+    elif args.model == "TST" or args.model == "TST_CNN" or args.model == "REINIT" or  args.model == "TSTEXP" or args.model == "TST_VIT" or args.model == "TST_ResNet50":
         lightning_module = TS_Module(model, num_classes, device=args.accelerator, freeze_qyx=args.freeze_qyx, dataset=args.dataset, loss=args.loss, gammas=gammas, probs=probs)
     elif args.model == "VTST" or args.model == "VTST_CNN" or args.model == "VTSTEXP" or args.model == "VTST_VIT":
         lightning_module = VTST_Module(model, num_classes, device=args.accelerator, freeze_qyx=args.freeze_qyx, dataset=args.dataset, loss=args.loss, gammas=gammas, probs=probs)
--- src/models/ClassYEncoders.py
+++ src/models/ClassYEncoders.py
@@ -9,9 +9,11 @@ from src.models.WRN import _BlockGroup
 from src.models.ViT import *
 from transformers import ViTForImageClassification, ViTImageProcessor, ViTModel, ViTConfig
 from data.tinyimagenet import *
+from src.models.ResNet_new import *
 
 WIDERESNET_WIDTH_WANG2023=10
 WIDERESNET_WIDTH_MNIST=4
+ResNet50_block_expansion = 4
 CIFAR10_MEAN = (0.4914, 0.4822, 0.4465)
 CIFAR10_STD = (0.2471, 0.2435, 0.2616)
 
@@ -167,14 +169,14 @@ class WRN2810Body(nn.Module):
         out = F.avg_pool2d(out, 8)
         out = out.view(-1, self.num_channels)
         return out
-    
+
 class CNNVarHead(nn.Module):
     def __init__(self, latent_dim: int = 128):
         super().__init__()
         #The input to the head is the output of the body which is 64*width (where width is the width of the ResNet).
         self.fc1 = nn.Linear(84, latent_dim*3)
         self.fc2 = nn.Linear(latent_dim*3, latent_dim)
-    
+
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.logsigmoid(self.fc2(x))
@@ -186,7 +188,7 @@ class CNNHead(nn.Module):
         #The input to the head is the output of the body which is 64*width (where width is the width of the ResNet).
         self.fc1 = nn.Linear(84, latent_dim*3)
         self.fc2 = nn.Linear(latent_dim*3, latent_dim)
-    
+
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
@@ -269,11 +271,68 @@ class ViTBody(nn.Module):
                         model_name_or_path,
                         config=config
                     )
-        #print("When initializing the ViTBody, these are the keys of the weights dict:")
-        #print(self.vit.state_dict().keys())
 
     def forward(self, x):
         outputs = self.vit(x)
         sequence_output = outputs[0]
 
         return sequence_output[:, 0, :]
+
+
+class ResNet50VarHead(nn.Module):
+    def __init__(self, latent_dim: int = 128):
+        super().__init__()
+        #The input to the head is the output of the body which is 64*width (where width is the width of the ResNet).
+        self.fc1 = nn.Linear(512*ResNet50_block_expansion, latent_dim*3)
+        self.fc2 = nn.Linear(latent_dim*3, latent_dim)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.logsigmoid(self.fc2(x))
+        return x
+
+class ResNet50Head(nn.Module):
+    def __init__(self, latent_dim: int = 128):
+        super().__init__()
+        #The input to the head is the output of the body which is 64*width (where width is the width of the ResNet).
+        self.fc1 = nn.Linear(512*ResNet50_block_expansion, latent_dim*3)
+        self.fc2 = nn.Linear(latent_dim*3, latent_dim)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+class ResNet50Body(nn.Module):
+    """Adapted ResNet50 model in which the last linear layer is removed"""
+    def __init__(self, block = Bottleneck , num_blocks = [3, 4, 6, 3], num_classes=10):
+        super(ResNet50Body, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        #self.linear = nn.Linear(512*block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        #out = self.linear(out)
+        return out
