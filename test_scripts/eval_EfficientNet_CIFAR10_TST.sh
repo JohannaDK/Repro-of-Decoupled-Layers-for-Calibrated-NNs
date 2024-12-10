2bf5c3773fec4c4f948c67a2b19dffa7230d4675
JohannaDK <johannadciofalo@gmail.com>
2024-12-10 15:19:06 +0000

    EfficientNet TST done

---
+++ test_scripts/eval_EfficientNet_CIFAR10_TST.sh
@@ -0,0 +1,9 @@
+#!/bin/bash	
+
+cd /home/johannakhodaverdian/DD2412-Final-Project/
+export PYTHONPATH=$PWD
+echo "!!Evaluating model!!"
+python3 src/experiments/01_eval_models.py \
+    --save_file_name EfficientNet_CIFAR10_TST_new.txt \
+    --model_name_file evaluate_EfficientNet_cifar10_tst.txt
+echo "!!Evaluation done!!"
