8a07a6a4dcac4a2018efa72625367bcce4721e41
JohannaDK <johannadciofalo@gmail.com>
2024-12-10 13:40:20 +0000

    ResNet50 done

--- train_scripts/train_ResNet50_CIFAR10_VTST_Z128.sh
+++ train_scripts/train_ResNet50_CIFAR10_VTST_Z128.sh
@@ -5,11 +5,11 @@ export PYTHONPATH=$PWD
 echo "!!Training model!!"
 python3 src/experiments/00_train_models.py \
     --freeze_qyx \
-    --model_name VTST_CIFAR10_Z32 \
+    --model_name VTST_CIFAR10_Z128 \
     --model VTST_ResNet50 \
     --epochs 40 \
     --accelerator gpu \
-    --latent_dim 32 \
+    --latent_dim 128 \
     --seed 0 \
     --pretrained_qyx experiment_results/CIFAR10_ResNet50/checkpoints/seed=1-epoch=194-valid_loss=0.566-model_name=ResNet50_CIFAR10_Base_seed1.ckpt \
     --dataset CIFAR10 \
